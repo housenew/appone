b5f81da47a1c1f1357cf7b89fba131fa97971e14
housenew <96601512+housenew@users.noreply.github.com>
2021-12-28 18:00:33 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+from data import *
+from input import image_input, webcam_input
+
+st.title("Ai Art for NFT Generation")
+st.sidebar.title('Input Source:')
+method = st.sidebar.radio('Select:', options=['Webcam', 'Image'])
+st.sidebar.header('Art Styles Available:')
+
+style_model_name = st.sidebar.selectbox("Select style for transformation: ", style_models_name)
+
+if method == 'Image':
+    image_input(style_model_name)
+else:
+    webcam_input(style_model_name)
