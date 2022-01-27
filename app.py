ab4751bc9bd9f92c4ea87169cef41fbf3c211819
housenew <96601512+housenew@users.noreply.github.com>
2022-01-27 01:03:08 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,14 @@ import streamlit as st
 
 from data import *
 from input import image_input, webcam_input
+
+
+st.title("Ai Art for NFT Generation")
+st.sidebar.title('Input Source:')
+method = st.sidebar.radio('Select:', options=['Webcam', 'Image'])
+st.sidebar.header('Art Styles Available:')
+
+style_model_name = st.sidebar.selectbox("Select style for transformation: ", style_models_name)
 st.header("ahoy")
 hide_st_style = """
                <style>
@@ -11,14 +19,6 @@ hide_st_style = """
                 </style>
                 """
 st.markdown(hide_st_style, unsafe_allow_html=True)
-
-st.title("Ai Art for NFT Generation")
-st.sidebar.title('Input Source:')
-method = st.sidebar.radio('Select:', options=['Webcam', 'Image'])
-st.sidebar.header('Art Styles Available:')
-
-style_model_name = st.sidebar.selectbox("Select style for transformation: ", style_models_name)
-
 if method == 'Image':
     image_input(style_model_name)
 else:
