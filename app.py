6849223619765ede74d3432043c039d85a7f1baa
housenew <96601512+housenew@users.noreply.github.com>
2022-01-27 01:16:38 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 import streamlit as st
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
 
 from data import *
 from input import image_input, webcam_input
 
-
+st.markdown(hide_st_style, unsafe_allow_html=True)
 st.title("Ai Art for NFT Generation")
 st.sidebar.title('Input Source:')
 method = st.sidebar.radio('Select:', options=['Webcam', 'Image'])
