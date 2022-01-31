0312823916013d8592adda0ae00f8e9af8ac15a3
housenew <96601512+housenew@users.noreply.github.com>
2022-01-30 20:15:57 -0500

    Update input.py

--- input.py
+++ input.py
@@ -38,7 +38,7 @@ def webcam_input(style_model_name):
     st.header("Webcam Live Feed")
     WIDTH = st.sidebar.select_slider('QUALITY (May reduce the speed)', list(range(150, 501, 50)))
     
-    result = st.button("Mint NFT",help="This feature is coming soon")
+    result = st.sidebar.button("Mint NFT",help="This feature is coming soon")
 
     
     class NeuralStyleTransferTransformer(VideoTransformerBase):
