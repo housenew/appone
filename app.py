987000a8057c6f715dc62a2087664f5e7023bc82
housenew <96601512+housenew@users.noreply.github.com>
2022-01-31 18:52:38 -0500

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from data import *
 from input import image_input, webcam_input
 
 st.markdown('''
-        <a href="javascript:document.getElementsByClassName('css-1ydp377 edgvbvh6')[1].click();">
+        <a href="javascript:document.getElementsByClassName('css-cuz8q9 edgvbvh5')[1].click();">
             <img src="https://www.freeiconspng.com/uploads/arrow-icon--myiconfinder-23.png" alt="Options" style="width:50px;height:50px;"/>
         </a>
         ''', unsafe_allow_html=True)
