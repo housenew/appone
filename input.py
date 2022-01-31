a2af5c749e43d2ebad56cca9c4666c20e94641b9
housenew <96601512+housenew@users.noreply.github.com>
2022-01-30 20:13:16 -0500

    Update input.py

--- input.py
+++ input.py
@@ -38,7 +38,7 @@ def webcam_input(style_model_name):
     st.header("Webcam Live Feed")
     WIDTH = st.sidebar.select_slider('QUALITY (May reduce the speed)', list(range(150, 501, 50)))
     
-    result = st.button("Mint NFT",help="This feature is coming soon", disabled="True")
+    result = st.button("Mint NFT",help="This feature is coming soon")
     if result:
              st.write("This feature is coming soon")
     
