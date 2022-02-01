11d9e71731d4b00313c1572c6bd79afe1560ac6c
housenew <96601512+housenew@users.noreply.github.com>
2022-01-31 20:13:02 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,16 +5,15 @@ hide_st_style = """
             footer {visibility: hidden;}
             header {visibility: hidden;}
             </style>
+            <a href="javascript:document.getElementsByClassName('css-cuz8q9 edgvbvh5')[1].click();">
+            <p><img src="https://www.freeiconspng.com/uploads/arrow-icon--myiconfinder-23.png" alt="Options" width="50" height="50" style="float:left">Show options</p>
+             </a>
             """
 
 from data import *
 from input import image_input, webcam_input
 
-st.markdown('''
-        <a href="javascript:document.getElementsByClassName('css-cuz8q9 edgvbvh5')[1].click();">
-            <p><img src="https://www.freeiconspng.com/uploads/arrow-icon--myiconfinder-23.png" alt="Options" width="50" height="50" style="float:left">Show options</p>
-        </a>
-        ''', unsafe_allow_html=True)
+st.markdown(hide_st_style, unsafe_allow_html=True)
 st.title("Ai Art for NFT Generation")
 st.sidebar.title('Input Source:')
 method = st.sidebar.radio('Select:', options=['Webcam', 'Image'])
